fad1162cc6fdca376b5228ec09ed3fb9aa702222
joaocansi <whitshs2003@gmail.com>
2025-03-18 20:55:52 -0300

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,42 +1,59 @@
+terraform {
+  required_providers {
+    render = {
+      source = "render-oss/render"
+      version = "1.6.0"
+    }
+    aiven = {
+      source = "aiven/aiven"
+    }
+  }
+}
+
+provider "aiven" {
+  api_token = var.aiven_api_token
+}
+
+resource "aiven_pg" "pg" {
+  project = var.aiven_project_name
+  cloud_name = "google-europe-west1"
+  plan = "hobbyist"
+  service_name = "comuni-dev"
+}
+
 resource "render_web_service" "web" {
+  depends_on = [ aiven_pg.pg ]
+
   name               = "terraform-web-service"
   plan               = "starter"
   region             = "oregon"
-  start_command      = "npm start"
-  pre_deploy_command = "echo 'hello world'"
+  start_command      = "npm run start:prod" 
 
   runtime_source = {
     native_runtime = {
       auto_deploy   = true
       branch        = "main"
-      build_command = "npm install"
+      build_command = "npm install --legacy-peer-deps && npm run build"
       build_filter = {
         paths         = ["src/**"]
-        ignored_paths = ["tests/**"]
       }
-      repo_url = "https://github.com/render-examples/express-hello-world"
+      repo_url = "https://github.com/joaocansi/comuni.dev-server"
       runtime  = "node"
     }
   }
 
   disk = {
-    name       = "some-disk"
+    name       = "comuni-dev"
     size_gb    = 1
     mount_path = "/data"
   }
 
   env_vars = {
-    "key1" = { value = "val1" },
-    "key2" = { value = "val2" },
+    "BETTER_AUTH_SECRET" = { value = var.better_auth_secret },
+    "DATABASE_URL" = { value = "postgresql://${aiven_pg.pg.service_username}:${aiven_pg.pg.service_password}@${aiven_pg.pg.service_host}:${aiven_pg.pg.service_port}/defaultdb?schema=public&sslmode=require" }
+    "GOOGLE_CLIENT_ID" = { value = var.google_client_id },
+    "GOOGLE_CLIENT_SECRET" = { value = var.google_client_secret  }
   }
-  secret_files = {
-    "file1" = { content = "content1" },
-    "file2" = { content = "content2" },
-  }
-  custom_domains = [
-    { name : "terraform-provider-1.example.com" },
-    { name : "terraform-provider-2.example.com" },
-  ]
 
   notification_override = {
     preview_notifications_enabled = "false"
@@ -46,5 +63,8 @@ resource "render_web_service" "web" {
   log_stream_override = {
     setting = "drop"
   }
+}
 
+output "url" {
+  value = render_web_service.web.url
 }
