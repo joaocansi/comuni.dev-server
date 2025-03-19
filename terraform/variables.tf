ee5cc9d704d326ef9af513b3e0c07b18dffeca6b
joaocansi <whitshs2003@gmail.com>
2025-03-18 22:42:27 -0300

    feat: add terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "aiven_api_token" {
+  type    = string
+  default = ""
+}
+
+variable "aiven_project_name" {
+  type    = string
+  default = ""
+}
+
+variable "google_client_id" {
+  type    = string
+  default = ""
+}
+
+variable "google_client_secret" {
+  type    = string
+  default = ""
+}
+
+variable "better_auth_secret" {
+  type    = string
+  default = ""
+}
